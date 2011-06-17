5308c4fa740ce41741af2deb75b45680f1470cc6
James Rowe <jnrowe@gmail.com>
2011-06-17 11:59:38 +0100

    Added task to open a new bug.

--- tasks/maintenance.py
+++ tasks/maintenance.py
@@ -1,9 +1,13 @@
 import datetime
 
 from glob import glob
+from re import search
+from subprocess import check_output
 
 from cake.helpers import task
 from cake.lib import puts
+from github2.client import Github
+
 
 @task('Check for missing keywords')
 def keyword_check():
@@ -26,3 +30,15 @@ def gen_stable(cpv):
     for arch in ('amd64', 'x86'):
         print "* %-40s %5s SCHEDULED: <%s>" \
               % (cpv, arch, date.strftime("%Y-%m-%d %a"))
+
+
+@task('Open a new bump bug')
+def open_bug(title, body="", label=None):
+    user = check_output('git config github.user'.split()).strip()
+    token = check_output('git config github.token'.split()).strip()
+    project_url = check_output('git config remote.origin.url'.split()).strip()
+    project = search('github.com[:/]([^/]+/.*).git', project_url).groups()[0]
+    github = Github(username=user, api_token=token)
+    new_issue = github.issues.open(project, title, body)
+    if label:
+        github.issues.add_label(project, new_issue.number, label)
