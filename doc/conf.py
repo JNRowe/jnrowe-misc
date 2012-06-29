f9f2817afb25234c8e46d193a7fa899dab5d199b
James Rowe <jnrowe@gmail.com>
2012-06-29 12:15:39 +0100

    Remove cloud_sptheme setup.
    The 'official' docs are now on rtfd.org, and this just adds an unneeded dep for local builds.

--- doc/conf.py
+++ doc/conf.py
@@ -14,11 +14,6 @@
 
 import os
 
-RTD = bool(os.getenv('READTHEDOCS'))
-
-if not RTD:
-    import cloud_sptheme as csp
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -100,10 +95,7 @@ pygments_style = 'emacs'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if RTD:
-    html_theme = 'default'
-else:
-    html_theme = 'cloud'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -113,8 +105,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-if not RTD:
-    html_theme_path = [csp.get_theme_dir(), ]
+html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
