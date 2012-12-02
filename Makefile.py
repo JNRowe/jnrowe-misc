05f847ac1c47754eee1497c567d8d5174ef1a147
James Rowe <jnrowe@gmail.com>
2012-12-02 19:51:17 +0000

    Added copyright notices for task code.

--- Makefile.py
+++ Makefile.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python -tt
-#
+# -*- coding: utf-8 -*-
 """Makefile - Overlay maintenance helpers"""
-# Copyright (C) 2009-2012  James Rowe <jnrowe@gmail.com>
+# Copyright (C) 2012  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
