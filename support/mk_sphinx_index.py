704a45fd2031b510a818f99721e70b1c8966fe83
James Rowe <jnrowe@gmail.com>
2013-01-02 09:30:06 +0000

    [support] Added support for virtual packages to mk_sphinx_index.

--- support/mk_sphinx_index.py
+++ support/mk_sphinx_index.py
@@ -109,12 +109,15 @@ for package, meta in sorted(CACHE.items()):
         output.append("``%s``" % cat)
         output.append("--" + "-" * len(cat) + "--")
     if not p == pkg:
-        if not meta["HOMEPAGE"] == "DEAD":
+        if not meta.get("HOMEPAGE") in ("DEAD", None):
             output.append('\n* ``%(NAME)s`` - %(HOMEPAGE)s' % meta)
         else:
             output.append('\n* ``%(NAME)s`` - no upstream' % meta)
         output.append('\n * %(DESCRIPTION)s' % meta)
-        output.append(' * Licence: ``%(LICENSE)s``' % meta)
+        if meta.get("LICENSE"):
+            output.append(' * Licence: ``%(LICENSE)s``' % meta)
+        else:
+            output.append(' * Licence: no source')
         output.append(' * Versions:\n')
         pkg = p
 
