JNRowe/jnrowe-misc

commit ba24e2ecefd3088a433ce7ca8f11662e71144f0c

[gpxviewer] Depend on upstream's osm-gps-map ebuild
Package-Manager: portage-2.1.11.55
Manifest-Sign-Key: C0174749

metadata/md5-cache/sci-geosciences/gpxviewer-0.4.3
@@ -4,10 +4,10 @@ DESCRIPTION=GPXViewer GPS trace viewer
 EAPI=5
 HOMEPAGE=http://andrewgee.org/blog/projects/gpxviewer
 IUSE=python_targets_python2_5 python_targets_python2_6 python_targets_python2_7
-KEYWORDS=amd64 x86
+KEYWORDS=~amd64 ~x86
 LICENSE=GPL-3+
 RDEPEND=dev-python/python-osmgpsmap[python_targets_python2_5(-)?,python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_5(-),-python_single_target_python2_6(-),-python_single_target_python2_7(-)] python_targets_python2_5? ( dev-lang/python:2.5 ) python_targets_python2_6? ( dev-lang/python:2.6 ) python_targets_python2_7? ( dev-lang/python:2.7 ) dev-python/python-exec[python_targets_python2_5(-)?,python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_5(-),-python_single_target_python2_6(-),-python_single_target_python2_7(-)]
 SLOT=0
 SRC_URI=https://github.com/andrewgee/gpxviewer/archive/0.4.3.tar.gz -> gpxviewer-0.4.3.tar.gz
 _eclasses_=distutils-r1	28f569122728e4a4b52d45c00fc45fff	eutils	36e6377b0e31856b6d62af831661c4f3	jnrowe-github	8fe80992e79232ebf59dd6f77adcdc09	multibuild	32fd04fe3ee97494341710886fd70dfe	multilib	892e597faee02a5b94eb02ab512e7622	multiprocessing	a2130e6fc4aa4c6a24b265ca0cbcc2b6	python-r1	052a2872c3d341495152eb93212374e9	python-utils-r1	1abc4a4828358a8fc1c4d65e74ae4a19	toolchain-funcs	69a2016af67775a812f4c03ba4b0e03e	user	d0a4d0735a6c0183d707ca919bd72f28	vcs-snapshot	3facff03591093044e38f21285a02129
-_md5_=e0f02e05056018f3713bfae73b6d772c
+_md5_=4dbbf3cd532cd2a8bf4dd6cf5bff0b7b

sci-geosciences/gpxviewer/gpxviewer-0.4.3.ebuild
@@ -15,7 +15,7 @@ HOMEPAGE="http://andrewgee.org/blog/projects/gpxviewer"
 
 LICENSE="GPL-3+"
 SLOT="0"
-KEYWORDS="amd64 x86"
+KEYWORDS="~amd64 ~x86"
 IUSE=""
 
 DEPEND="dev-python/python-distutils-extra

support/stabilisation.rem
@@ -43,6 +43,8 @@ REM 2013-05-24 *1 MSG %"Stabilise amd64 sci-visualization/pycha-0.7.0%" %a
 REM 2013-05-24 *1 MSG %"Stabilise x86 sci-visualization/pycha-0.7.0%" %a
 REM 2013-05-24 *1 MSG %"Stabilise amd64 app-misc/hubugs-0.17.2%" %a
 REM 2013-05-24 *1 MSG %"Stabilise x86 app-misc/hubugs-0.17.2%" %a
+REM 2013-05-24 *1 MSG %"Stabilise amd64 sci-geosciences/gpxviewer-0.4.3%" %a
+REM 2013-05-24 *1 MSG %"Stabilise x86 sci-geosciences/gpxviewer-0.4.3%" %
 REM 2013-05-26 *1 MSG %"Stabilise amd64 app-misc/Hammertime-0.2.4%" %a
 REM 2013-05-26 *1 MSG %"Stabilise amd64 dev-util/ditz-0.5-r1%" %a
 REM 2013-05-26 *1 MSG %"Stabilise amd64 dev-vcs/gitflow-0.4.1-r2%" %a

commit 241f6674e8a86deee2e91ed89a9814dfbe7b9ef5

[gpxviewer] Fix osmgpsmap dependency for upstream compatibility
This corrects the broken update in ba24e2ecefd3088a433ce7ca8f11662e71144f0c.

Package-Manager: portage-2.1.11.62
Manifest-Sign-Key: C0174749

metadata/md5-cache/sci-geosciences/gpxviewer-0.4.3
@@ -1,14 +1,14 @@
 DEFINED_PHASES=compile configure install prepare test unpack
-DEPEND=dev-python/python-distutils-extra dev-util/intltool python_targets_python2_5? ( dev-lang/python:2.5 ) python_targets_python2_6? ( dev-lang/python:2.6 ) python_targets_python2_7? ( dev-lang/python:2.7 ) dev-python/python-exec[python_targets_python2_5(-)?,python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_5(-),-python_single_target_python2_6(-),-python_single_target_python2_7(-)]
+DEPEND=dev-python/python-distutils-extra dev-util/intltool python_targets_python2_6? ( dev-lang/python:2.6 ) python_targets_python2_7? ( dev-lang/python:2.7 ) dev-python/python-exec[python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_6(-),-python_single_target_python2_7(-)]
 DESCRIPTION=GPXViewer GPS trace viewer
 EAPI=5
 HOMEPAGE=http://andrewgee.org/blog/projects/gpxviewer
-IUSE=python_targets_python2_5 python_targets_python2_6 python_targets_python2_7
+IUSE=python_targets_python2_6 python_targets_python2_7
 KEYWORDS=~amd64 ~x86
 LICENSE=GPL-3+
-RDEPEND=dev-python/python-osmgpsmap[python_targets_python2_5(-)?,python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_5(-),-python_single_target_python2_6(-),-python_single_target_python2_7(-)] python_targets_python2_5? ( dev-lang/python:2.5 ) python_targets_python2_6? ( dev-lang/python:2.6 ) python_targets_python2_7? ( dev-lang/python:2.7 ) dev-python/python-exec[python_targets_python2_5(-)?,python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_5(-),-python_single_target_python2_6(-),-python_single_target_python2_7(-)]
-REQUIRED_USE=|| ( python_targets_python2_5 python_targets_python2_6 python_targets_python2_7 )
+RDEPEND=sci-geosciences/osm-gps-map[python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_6(-),-python_single_target_python2_7(-)] python_targets_python2_6? ( dev-lang/python:2.6 ) python_targets_python2_7? ( dev-lang/python:2.7 ) dev-python/python-exec[python_targets_python2_6(-)?,python_targets_python2_7(-)?,-python_single_target_python2_6(-),-python_single_target_python2_7(-)]
+REQUIRED_USE=|| ( python_targets_python2_6 python_targets_python2_7 )
 SLOT=0
 SRC_URI=https://github.com/andrewgee/gpxviewer/archive/0.4.3.tar.gz -> gpxviewer-0.4.3.tar.gz
 _eclasses_=distutils-r1	6950481ecc2ad548f2d9d116a0936fb8	eutils	f31a0ec0d081047cbf9c0bbb4822d831	jnrowe-github	8fe80992e79232ebf59dd6f77adcdc09	multibuild	32fd04fe3ee97494341710886fd70dfe	multilib	892e597faee02a5b94eb02ab512e7622	multiprocessing	a2130e6fc4aa4c6a24b265ca0cbcc2b6	python-r1	094dc7421f9aea7525b85b899f67e62a	python-utils-r1	9fc80a4f06f33ede447b5647fddca301	toolchain-funcs	7ffd28a8c7eea27218865352bfd3ab2f	user	d0a4d0735a6c0183d707ca919bd72f28	vcs-snapshot	3facff03591093044e38f21285a02129
-_md5_=4dbbf3cd532cd2a8bf4dd6cf5bff0b7b
+_md5_=ac3c88758648d6e20527bcac7ea52cfd

sci-geosciences/gpxviewer/gpxviewer-0.4.3.ebuild
@@ -4,7 +4,7 @@
 EAPI=5
 
 # 3.x is restricted due to lack of support in python-distutils-extra
-PYTHON_COMPAT=(python2_{5..7})
+PYTHON_COMPAT=(python2_{6,7})
 
 GITHUB_USER=andrewgee
 
@@ -20,6 +20,6 @@ IUSE=""
 
 DEPEND="dev-python/python-distutils-extra
 	dev-util/intltool"
-RDEPEND="dev-python/python-osmgpsmap[${PYTHON_USEDEP}]"
+RDEPEND="sci-geosciences/osm-gps-map[${PYTHON_USEDEP}]"
 
 PATCHES=("${FILESDIR}"/${P}-valid_desktop.patch)