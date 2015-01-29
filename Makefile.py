a32ca5c44e39dc9e7a401f574dbc66bfeec89917
James Rowe <jnrowe@gmail.com>
2015-01-29 23:51:20 +0000

    [Tr] Bump copyright years

--- Makefile.py
+++ Makefile.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python -tt
 # -*- coding: utf-8 -*-
 """Makefile - Overlay maintenance helpers"""
-# Copyright © 2009, 2010, 2011, 2012, 2013, 2014  James Rowe <jnrowe@gmail.com>
+# Copyright © 2009-2015  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- tasks/layman_support.py
+++ tasks/layman_support.py
@@ -1,7 +1,7 @@
 #
 # -*- coding: utf-8 -*-
 """layman_support - layman support taks"""
-# Copyright © 2009, 2010, 2011, 2012, 2013, 2014  James Rowe <jnrowe@gmail.com>
+# Copyright © 2009-2015  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
